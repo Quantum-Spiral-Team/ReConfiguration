564d1d04b635f2efb5a58da8002863d4e05ebd27
Bronit <suslinv037@gmail.com>
2026-06-22 14:17:11 +0300

    remove unused field `DEPENDENCIES`

--- src/main/java-templates/com/qsteam/reconf/Reference.java
+++ src/main/java-templates/com/qsteam/reconf/Reference.java
@@ -9,7 +9,6 @@ public class Reference {
     public static final String MOD_ID = "{{ mod_id }}";
     public static final String MOD_NAME = "{{ mod_name }}";
     public static final String VERSION = "{{ mod_version }}";
-    public static final String DEPENDENCIES = "{{ mod_dependencies }}";
     public static final String CLIENT_PROXY_CLASS = "{{ client_proxy_class }}";
     public static final String SERVER_PROXY_CLASS = "{{ server_proxy_class }}";
 }
