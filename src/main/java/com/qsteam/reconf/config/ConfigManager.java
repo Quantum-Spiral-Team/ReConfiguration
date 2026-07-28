8a4257ce9befa69abfae81a87e2f2b14fdb36f5f
Bronit <suslinv037@gmail.com>
2026-07-28 07:57:27 +0300

    update ConfigManager methods

--- src/main/java/com/qsteam/reconf/config/ConfigManager.java
+++ src/main/java/com/qsteam/reconf/config/ConfigManager.java
@@ -3,11 +3,17 @@ package com.qsteam.reconf.config;
 import com.qsteam.reconf.api.ReConfig;
 import com.qsteam.reconf.util.LogUtil;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.common.discovery.ASMDataTable;
 import org.slf4j.Logger;
 
 public class ConfigManager {
 
     public static final Logger LOGGER = LogUtil.getLogger(ConfigManager.class);
+
+    /** for {@link ReConfig.Type#INSTANCE} */
+    public static void loadData(ASMDataTable data) {
+        //TODO
+    }
 
     /** for {@link ReConfig.Type#PER_WORLD} */
     public static void sync(String modId, World world) {
@@ -21,6 +27,10 @@ public class ConfigManager {
 
     /** for {@link ReConfig.Type#LAZY} */
     public static void sync(Class<?> clazz) {
+        //TODO
+    }
+
+    public static void register(Class<?> clazz) {
         //TODO
     }
 }
