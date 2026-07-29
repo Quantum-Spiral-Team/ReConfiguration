11bab4261437299d14662a57e14a99bf07dd53e6
Bronit <suslinv037@gmail.com>
2026-07-29 11:16:46 +0300

    update ConfigMeta

--- src/main/java/com/qsteam/reconf/config/meta/ConfigMeta.java
+++ src/main/java/com/qsteam/reconf/config/meta/ConfigMeta.java
@@ -1,31 +1,28 @@
 package com.qsteam.reconf.config.meta;
 
 import com.qsteam.reconf.api.ReConfig;
-import org.jetbrains.annotations.Nullable;
 
+import java.io.File;
 import java.util.List;
 
 public record ConfigMeta(
         Class<?> configClass,
         String modId,
-        String name,
-        String dir,
+        File file,
         ReConfig.Type type,
         String rootCategory,
         List<FieldMeta> fields
 ) {
     public ConfigMeta(Class<?> configClass,
                       String modId,
-                      @Nullable String name,
-                      String dir,
+                      File file,
                       ReConfig.Type type,
                       String rootCategory,
                       List<FieldMeta> fields
     ) {
         this.configClass = configClass;
         this.modId = modId;
-        this.name = name == null || name.isEmpty() ? modId : name;
-        this.dir = dir;
+        this.file = file;
         this.type = type;
         this.rootCategory = rootCategory;
         this.fields = List.copyOf(fields);
