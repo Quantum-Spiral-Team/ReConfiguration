b6780973dc8666a887690dbc99542fbb782db69e
Bronit <suslinv037@gmail.com>
2026-08-20 00:12:34 +0300

    update buildscript

--- src/main/java-templates/com/qsteam/reconf/Reference.java
+++ src/main/java-templates/com/qsteam/reconf/Reference.java
@@ -1,14 +1,15 @@
-package {{ package }};
+package {{ root_package }}.{{ mod_id }};
 
 /**
  * Tags storage class, you can change at will
  */
-public class Reference {
+public final class Reference {
     private Reference() {}
 
     public static final String MOD_ID = "{{ mod_id }}";
     public static final String MOD_NAME = "{{ mod_name }}";
     public static final String VERSION = "{{ mod_version }}";
-    public static final String CLIENT_PROXY_CLASS = "{{ client_proxy_class }}";
-    public static final String SERVER_PROXY_CLASS = "{{ server_proxy_class }}";
+    public static final String CLIENT_PROXY_CLASS = "{{ root_package }}.{{ mod_id }}.proxy.ClientProxy";
+    public static final String SERVER_PROXY_CLASS = "{{ root_package }}.{{ mod_id }}.proxy.CommonProxy";
+
 }
